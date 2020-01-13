1ca6368bee50bd899cab6b8a98b5ef8977fe43b3
Bruno Silva <brunosilvinosilva@gmail.com>
2020-01-13 14:16:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,9 +17,11 @@ summon_captain_planet(planeteer_calls)
 short_words = ["puff", "go", "two"]
 assorted_words = ["two", "go", "industrious", "bop"]
 def long_planeteer_calls(array)
-  array.any? 
-
+  array.any? do |word|
+    word.length > 4 
+  end 
 end
+long_planeteer_calls
 
 def find_the_cheese# code an argument here
   # the array below is here to help
