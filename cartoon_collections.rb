d588ea11ea65a9410c73028e5f61f97125d592d5
Bruno Silva <brunosilvinosilva@gmail.com>
2020-01-13 13:55:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,6 @@
 dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"] 
 def roll_call_dwarves(array)
-  i = 0 
-  while i < array.length 
-    puts "#{i + 1}. #{array[i]}"
-    i += 1 
-  end 
+  
 end
 roll_call_dwarves(dwarves)
 
