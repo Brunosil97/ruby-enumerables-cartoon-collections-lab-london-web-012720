ce3bc91beea85608b29861694b40c39d9d25d856
Bruno Silva <brunosilvinosilva@gmail.com>
2020-01-13 13:56:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,7 @@ roll_call_dwarves(dwarves)
 
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 def summon_captain_planet(planeteer_calls)
-  i = 0 
-  while i < 
+  
 end
 
 def long_planeteer_calls# code an argument here
