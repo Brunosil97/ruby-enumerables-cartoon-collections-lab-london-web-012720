6902a92e4f937a28b1e374291432e3b2189bd230
Bruno Silva <brunosilvinosilva@gmail.com>
2020-01-13 13:59:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"] 
-def roll_call_dwarves(dwarves)
-  dwarves.each_with_index do |index, name| 
+def roll_call_dwarves(array)
+  array.each_with_index do |index, name| 
     puts "#{index+1}. #{name}"
   end 
 end
